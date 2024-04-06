6c59be510efca90f1ec8cbee7d23c07978a8e6c6
vasubhalodiya <vasubhalodiya30@gmail.com>
2024-04-07 00:23:43 +0530

    update

--- js/script.js
+++ js/script.js
@@ -94,14 +94,26 @@ function closeDialog() {
       })
     })
 
+// ======================== category filter =================================
+const accordionBtns = document.querySelectorAll(".filter-list button");
 
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("is-open");
 
+    let content = this.nextElementSibling;
+    console.log(content);
 
-
-
-
-
-
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
 
 
 

3b3b9c2bb6cc4dfe144507c33212dc9a041333b3
vasubhalodiya <vasubhalodiya30@gmail.com>
2024-04-21 15:52:18 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,83 +1,80 @@
 // ========================= Navbar =========================
 const showMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId);
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
 
-  toggle.addEventListener("click", () => {
-    nav.classList.toggle("show-menu");
+    toggle.addEventListener("click", () => {
+        nav.classList.toggle("show-menu");
 
-    toggle.classList.toggle("show-icon");
-  });
+        toggle.classList.toggle("show-icon");
+    });
 };
 
 showMenu("nav-toggle", "nav-menu");
 
 //  ========================= slider =========================
 
-$(document).ready(function () {
-  $(".happy-customer-slider").slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: true,
-    autoplay: true,
-    infinite: true,
-    autoplaySpeed: 50000,
-    responsive: [
-      {
-        breakpoint: 766,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 575,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
-  $(".prev-btn").click(function () {
-    $(".happy-customer-slider").slick("slickPrev");
-  });
+// $(document).ready(function () {
+//     $(".happy-customer-slider").slick({
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//         arrows: true,
+//         autoplay: true,
+//         infinite: true,
+//         autoplaySpeed: 50000,
+//         responsive: [
+//             {
+//                 breakpoint: 1024,
+//                 settings: {
+//                     slidesToShow: 2,
+//                 },
+//             },
+//             {
+//                 breakpoint: 600,
+//                 settings: {
+//                     slidesToShow: 2,
+//                 },
+//             },
+//             {
+//                 breakpoint: 480,
+//                 settings: {
+//                     slidesToShow: 1,
+//                 },
+//             },
+//         ],
+//     });
+//     $(".prev-btn").click(function () {
+//         $(".happy-customer-slider").slick("slickPrev");
+//     });
 
-  $(".next-btn").click(function () {
-    $(".happy-customer-slider").slick("slickNext");
-  });
-  $(".prev-btn").addClass("slick-disabled");
-  $(".happy-customer-slider").on("afterChange", function () {
-    if ($(".slick-prev").hasClass("slick-disabled")) {
-      $(".prev-btn").addClass("slick-disabled");
-    } else {
-      $(".prev-btn").removeClass("slick-disabled");
-    }
-    if ($(".slick-next").hasClass("slick-disabled")) {
-      $(".next-btn").addClass("slick-disabled");
-    } else {
-      $(".next-btn").removeClass("slick-disabled");
-    }
-  });
-});
+//     $(".next-btn").click(function () {
+//         $(".happy-customer-slider").slick("slickNext");
+//     });
+//     $(".prev-btn").addClass("slick-disabled");
+//     $(".happy-customer-slider").on("afterChange", function () {
+//         if ($(".slick-prev").hasClass("slick-disabled")) {
+//             $(".prev-btn").addClass("slick-disabled");
+//         } else {
+//             $(".prev-btn").removeClass("slick-disabled");
+//         }
+//         if ($(".slick-next").hasClass("slick-disabled")) {
+//             $(".next-btn").addClass("slick-disabled");
+//         } else {
+//             $(".next-btn").removeClass("slick-disabled");
+//         }
+//     });
+// });
 
 // ========================= sign up dialog box close =========================
 
 const dialog = document.getElementById("myDialog");
 
 function showDialog() {
-  dialog.show();
+    dialog.show();
 }
 
 function closeDialog() {
-  dialog.close();
+    dialog.close();
 }
 
 // ================================= client section =================================
@@ -86,79 +83,79 @@ const tabs = document.querySelectorAll(".client-link");
 const allctn = document.querySelectorAll(".client-tab-cnt");
 
 tabs.forEach((tab, index) => {
-  tab.addEventListener("click", () => {
-    tabs.forEach((tab) => {
-      tab.classList.remove("active");
+    tab.addEventListener("click", () => {
+        tabs.forEach((tab) => {
+            tab.classList.remove("active");
+        });
+        tab.classList.add("active");
+        allctn.forEach((content) => {
+            content.classList.remove("active");
+        });
+        allctn[index].classList.add("active");
     });
-    tab.classList.add("active");
-    allctn.forEach((content) => {
-      content.classList.remove("active");
-    });
-    allctn[index].classList.add("active");
-  });
 });
 
 // ======================== category filter =================================
 const accordionBtns = document.querySelectorAll(".filter-list button");
 
 accordionBtns.forEach((accordion) => {
-  accordion.onclick = function () {
-    this.classList.toggle("is-open");
+    accordion.onclick = function () {
+        this.classList.toggle("is-open");
 
-    let content = this.nextElementSibling;
-    console.log(content);
+        let content = this.nextElementSibling;
+        console.log(content);
 
-    if (content.style.maxHeight) {
-      //this is if the accordion is open
-      content.style.maxHeight = null;
-    } else {
-      //if the accordion is currently closed
-      content.style.maxHeight = content.scrollHeight + "px";
-      console.log(content.style.maxHeight);
-    }
-  };
+        if (content.style.maxHeight) {
+            //this is if the accordion is open
+            content.style.maxHeight = null;
+        } else {
+            //if the accordion is currently closed
+            content.style.maxHeight = content.scrollHeight + "px";
+            console.log(content.style.maxHeight);
+        }
+    };
 });
 
 // ============================ price slider ===============================
 
 const rangeInput = document.querySelectorAll(".range-input input"),
-  priceInput = document.querySelectorAll(".price-input input"),
-  range = document.querySelector(".price-slider .progress");
+    priceInput = document.querySelectorAll(".price-input input"),
+    range = document.querySelector(".price-slider .progress");
 let priceGap = 1000;
 
 priceInput.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    let minPrice = parseInt(priceInput[0].value),
-      maxPrice = parseInt(priceInput[1].value);
+    input.addEventListener("input", (e) => {
+        let minPrice = parseInt(priceInput[0].value),
+            maxPrice = parseInt(priceInput[1].value);
 
-    if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
-      if (e.target.className === "input-min") {
-        rangeInput[0].value = minPrice;
-        range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
-      } else {
-        rangeInput[1].value = maxPrice;
-        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
-      }
-    }
-  });
+        if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+            if (e.target.className === "input-min") {
+                rangeInput[0].value = minPrice;
+                range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+            } else {
+                rangeInput[1].value = maxPrice;
+                range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+            }
+        }
+    });
 });
 
 rangeInput.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    let minVal = parseInt(rangeInput[0].value),
-      maxVal = parseInt(rangeInput[1].value);
+    input.addEventListener("input", (e) => {
+        let minVal = parseInt(rangeInput[0].value),
+            maxVal = parseInt(rangeInput[1].value);
 
-    if (maxVal - minVal < priceGap) {
-      if (e.target.className === "range-min") {
-        rangeInput[0].value = maxVal - priceGap;
-      } else {
-        rangeInput[1].value = minVal + priceGap;
-      }
-    } else {
-      priceInput[0].value = minVal;
-      priceInput[1].value = maxVal;
-      range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
-      range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
-    }
-  });
+        if (maxVal - minVal < priceGap) {
+            if (e.target.className === "range-min") {
+                rangeInput[0].value = maxVal - priceGap;
+            } else {
+                rangeInput[1].value = minVal + priceGap;
+            }
+        } else {
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    });
 });
